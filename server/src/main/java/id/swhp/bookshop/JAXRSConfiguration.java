b7e912c012afb753588c1f474cc22d61af3205a3
swhp <sukma.wardana@mail.com>
2017-04-14 11:17:38 +0700

    create JAX-RS Configuration

---
+++ server/src/main/java/id/swhp/bookshop/JAXRSConfiguration.java
@@ -0,0 +1,14 @@
+package id.swhp.bookshop;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author Sukma Wardana
+ * @since 1.0.0
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+
+}
