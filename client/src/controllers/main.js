b84353a47059805daacee41b94f78f86dd0135b6
swhp <sukma.wardana@mail.com>
2017-04-15 08:00:53 +0700

    initial main pages

---
+++ client/src/controllers/main.js
@@ -0,0 +1,22 @@
+(function() {
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bookshopApp:controller:MainCtrl
+ * @description
+ * # MainCtrl for Main Page
+ */
+
+angular.module('bookshopApp')
+    .controller('MainCtrl', function($log, $scope, $rootScope, BookService) {
+        
+        BookService.query(function(books) {
+            $scope.books = books;
+        }, function(error) {
+            // Broadcast even for server error
+            $rootScope.$broadcast('error');
+        });
+        
+    });
+})();
